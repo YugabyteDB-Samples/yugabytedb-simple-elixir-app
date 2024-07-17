d98099f45d932c6c41bb1432b6853fe5211bc28e
Denis Magda <dmagda@gridgain.com>
2024-07-17 10:14:45 -0400

    removing the Phoenix stub

--- phoenix/simple_app/lib/simple_app/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule SimpleApp.Mailer do
-  use Swoosh.Mailer, otp_app: :simple_app
-end
--- phoenix/simple_app/lib/simple_app/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule SimpleApp.Repo do
-  use Ecto.Repo,
-    otp_app: :simple_app,
-    adapter: Ecto.Adapters.Postgres
-end
